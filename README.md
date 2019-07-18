9719f2da5ca9439f90f79dea1830d8912976cc47
museun <museun@outlook.com>
2019-07-18 14:56:47 -0400

    output json and git branch

--- README.md
+++ README.md
@@ -1,22 +1,74 @@
 # shortpath
 takes a path and shortens it, similar to what fish does
 
+also provides the current git branch, if desired
+
+### usage:
+```
+Usage: shortpath [OPTIONS]
+
+Positional arguments:
+  path
+
+Optional arguments:
+  -h, --help        print help message
+  -g, --git-branch  display current git branch
+  -j, --json bool   output as json (default: true)
+```
+
 ### example:
+**note** (a blank newline is added for easier parsing)
+>shortpath -j false "c:/some long/path with/multiple/directories/here"
 ```
-shortpath "c:/some long/path with/multiple/directories/here"
-
 c:/s/p/m/d/here
+
 ```
 
-this is useful for shortening paths for a shell prompt 
+>shortpath "c:/some long/path with/multiple/directories/here"
+```json
+{"branch":"","path":"c:/s/p/m/d/here"}
+```
+
+..and pretend we're in a repo with the current branch as dev
+>shortpath "c:/some long/path with/multiple/directories/here" -g
+```json
+{"branch":"dev","path":"c:/s/p/m/d/here"}
+```
+
+**note** (a blank newline is added for easier parsing)
+>shortpath "c:/some long/path with/multiple/directories/here" -g -j false
+```json
+c:/s/p/m/d/here
+dev
+
+```
+
+this is useful for shortening paths for a shell prompt
 
 e.g. for a powershell profile:
 ```powershell
 # in $Profile.ps1
 function prompt {
-    $path = shortpath (get-location) # shortpath.exe in your $env:PATH
+    write-host "[ " -nonewline -foregroundcolor blue
+    $temp = ConvertFrom-Json (shortpath -g (get-location)) # shortpath.exe in your $env:PATH
+
+    $path = $temp.path;
+    $branch = $temp.branch.trim();
+
     write-host $path -nonewline -foregroundcolor green
-    write-host ">" -nonewline -foregroundcolor blue
+    $exit = $LASTEXITCODE
+    if ($null -ne $exit -and $exit -ne 0) {
+        write-host "!" -nonewline -foregroundcolor yellow
+        $exit = "{0:X}" -f $exit
+        write-host "$exit" -nonewline -foregroundcolor red
+    }
+
+    if ($null -ne $branch -and "" -ne $branch) {
+        write-host " @ " -NoNewline -foregroundcolor cyan
+        write-host $branch -NoNewline -ForegroundColor Magenta
+    }
+
+    write-host " ]" -nonewline -foregroundcolor blue
     return " "
 }
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+use gumdrop::Options;
+use lexical_bool::LexicalBool;
+
+#[derive(Clone, Options, Debug)]
+struct Args {
+    #[options(help = "print help message")]
+    help: bool,
+
+    #[options(help = "display current git branch")]
+    git_branch: bool,
+
+    #[options(help = "output as json", meta = "bool", default = "true")]
+    json: LexicalBool,
+
+    #[options(free, required)]
+    path: String,
+}
+
 fn main() {
-    let path = match std::env::args().nth(1) {
-        Some(path) => path,
-        None => std::process::exit(1),
-    };
+    let args = Args::parse_args_default_or_exit();
 
     use std::path::{Component::*, Path};
-    let parts: Vec<_> = Path::new(&path)
+    let parts: Vec<_> = Path::new(&args.path)
         .components()
         .filter_map(|k| match k {
             Normal(p) => Some(p.to_string_lossy()),
@@ -18,14 +33,49 @@ fn main() {
         std::process::exit(1)
     }
 
+    let mut string = String::new();
+
     let mut iter = parts.iter();
-    print!("{}/", iter.next().unwrap());
+    string.push_str(iter.next().unwrap());
+    string.push('/');
+
     if parts.len() > 2 {
         iter.take(parts.len() - 2)
             .filter_map(|k| k.chars().next())
-            .for_each(|c| print!("{}/", c));
+            .for_each(|c| {
+                string.push(c);
+                string.push('/');
+            });
     }
     if parts.len() > 1 {
-        print!("{}", parts.last().unwrap())
+        string.push_str(&parts.last().unwrap());
     }
+
+    let branch = if args.git_branch {
+        get_git_branch().unwrap_or_default()
+    } else {
+        "".into()
+    };
+
+    if *args.json {
+        println!(
+            "{}",
+            serde_json::to_string(&serde_json::json!({
+                "path": string,
+                "branch": branch.trim(),
+            }))
+            .expect("valid json")
+        );
+    } else {
+        println!("{}", string);
+        println!("{}", branch);
+    }
+}
+
+fn get_git_branch() -> Option<String> {
+    std::process::Command::new("git")
+        .args(&["rev-parse", "--abbrev-ref", "HEAD"])
+        .output()
+        .ok()
+        .and_then(|s| String::from_utf8(s.stdout).ok())
 }
